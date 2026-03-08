769710c23e10fd1a678e819ebd8cfd11cf5417ee
leesanggyu41 <uberdog4168@gmail.com>
2026-03-09 08:29:20 +0900

    .

---
+++ Trial/Assets/Scripts/Server/LobbyManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using Fusion;
+using System;
+using UnityEngine.UI;
+
+public class LobbyManager : MonoBehaviour
+{
+    public InputField RoomNameField;
+    public  InputField passwordField;
+    public String RoomName;
+    public String Roompassword;
+    public GameObject CreateRoom;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    public void OpenCreateRoom()
+    {
+    
+    }
+    public void CloudCreateRoom()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
